c9743613448258a3f2ba2c345b5d0ed0b9868b82
yoshihiro_kawabata <yoshihiro_kawabata@mediado.jp>
2022-04-18 14:13:58 +0900

    ログイン機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,8 +47,4 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
-  def correct_user
-      @user = User.find(params[:id])
-      redirect_to current_user unless current_user?(@user)
-  end
 end
